9c1bfe09970343916731831948c0b19385ff383e
Piotr Gawron <piotr.gawron@uni.lu>
2019-12-08 19:45:01 +0100

    data is fetched over API

--- minerva-basic.js
+++ minerva-basic.js
@@ -1,17 +1,56 @@
 /**
- *
- * @param {Object} params
- * @param {String} params.elementId identifier of HTML tag where map wil be visualized
- * @return {Map}
+ * Due to CORS we need to use proxy.
+ * @param url
+ * @return {Promise<Response>}
  */
-function createMap(params) {
+async function fetchOverProxy(url) {
+  return await fetch("https://minerva-dev.lcsb.uni.lu/minerva-proxy/?url=" + url);
+}
+
+/**
+ *
+ * @param elementId identifier of HTML tag where map wil be visualized
+ * @param projectId identifier of the project
+ * @param submapId identifier of the submap
+ * @param serverUrl url to minerva location (for example https://pdmap.uni.lu/minerva/)
+ */
+async function createMap({elementId, projectId, submapId, serverUrl}) {
+  let apiUrl = serverUrl + "api/";
+
+  let response = await fetchOverProxy(apiUrl + "projects/" + projectId);
+  let projectData = await response.json();
+
+  let projectDirectory = serverUrl + "../map_images/" + projectData.directory;
+
+  response = await fetchOverProxy(apiUrl + "projects/" + projectId + "/models/" + submapId);
+  let submapData = await response.json();
+
+  let height = submapData.height;
+  let width = submapData.width;
+  let minZoom = submapData.minZoom;
+  let maxZoom = submapData.maxZoom;
+
+  response = await fetchOverProxy(apiUrl + "projects/" + projectId + "/overlays/");
+  let overlaysData = await response.json();
+
+  overlaysData = overlaysData.filter(function (el) {
+    return el.name === "Network";
+  });
+
+  let imagesData = overlaysData[0].images.filter(function (el) {
+    return el.modelId === submapId;
+  });
+
+  let overlayDirectory = projectDirectory + "/" + imagesData[0].path + "/";
+
+
   return new ol.Map({
-    target: params.elementId,
+    target: elementId,
     layers: [
       new ol.layer.Tile({
         source: new ol.source.XYZ({
-          minZoom: 2, //TODO
-          maxZoom: 9, //TODO
+          minZoom: minZoom,
+          maxZoom: maxZoom,
           wrapX: false,
           tileLoadFunction: function (imageTile, src) {
             if (src !== null) {
@@ -22,15 +61,13 @@ function createMap(params) {
           tileUrlFunction: function (coordinate) {
             let zoom = coordinate[0];
             // we have 1 tile on MIN_ZOOM and therefore must limit tails according to this
-            let maxTileRange = 1 << (zoom - 2); //TODO minZoom
+            let maxTileRange = 1 << (zoom - minZoom);
             let maxTileXRange = maxTileRange;
             let maxTileYRange = maxTileRange;
 
             let x = coordinate[1];
             let y = coordinate[2];
 
-            let width = 28045; //TODO
-            let height = 13644; //TODO
             if (width > height) {
               maxTileYRange = height / width * maxTileRange;
             } else if (width < height) {
@@ -40,13 +77,13 @@ function createMap(params) {
               return null;
             }
 
-            return "https://pdmap.uni.lu/map_images/1cc799ade846d30a2e742089e72e2484/_normal0/" + zoom + "/" + x + "/" + y + ".PNG"; //TODO
+            return overlayDirectory + zoom + "/" + x + "/" + y + ".PNG";
           }
         })
       })
     ],
     view: new ol.View({
-      center: ol.proj.fromLonLat([-120, 90]),//TODO
+      center: ol.proj.fromLonLat([-120, 90]), //TODO this should be automatically centered
       zoom: 4
     })
   });
